98152d9e78eae1d1beb51e170336408d5725c482
ronan-cros <ronan.cros@gmail.com>
2026-07-08 16:42:26 +0930

    Fix duplicate for loops in LED blinking code

--- 03-LEDCircuit/Code.ino
+++ 03-LEDCircuit/Code.ino
@@ -7,19 +7,19 @@ pinMode(12,OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
-for(int i = 0; i < 5; i++) {
+for(int i = 0; i < 5; i++) { //Blink the LED connected to pin 12 five times
   digitalWrite(12,HIGH);
   delay(500);
   digitalWrite(12,LOW);
   delay(500);
 }
-for(int i = 0; i < 10; i++) {
+for(int i = 0; i < 10; i++) { //Blink the LED connected to pin 11 ten times
   digitalWrite(11,HIGH);
   delay(500);
   digitalWrite(11,LOW);
   delay(500);
 }
-for(int i = 0; i < 15; i++) {
+for(int i = 0; i < 15; i++) { //Blink the LED connected to pin 10 fifteen times
   digitalWrite(10,HIGH);
   delay(500);
   digitalWrite(10,LOW);
